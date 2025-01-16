5d458b7b2856c00011bf4ecf3609956851d37199
jourialem123 <alemjouri@gmail.com>
2025-01-16 20:13:00 +0300

    activity1

---
+++ activity1.py
@@ -0,0 +1,10 @@
+#Write a Python program first to take a number as an input from the user,
+#  then check whether that number is even or odd and print the result
+
+num=int(input("Enter number:"))
+
+if num%2==0:
+    print("The number ",num," is even")
+
+else:
+    print("The number ",num," is odd")
